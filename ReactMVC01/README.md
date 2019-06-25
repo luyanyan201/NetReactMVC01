612b42101e0198405992aca9977ffa100e7c9a4a
luyanyan201 <luyanyan201@gmail.com>
2019-06-26 03:51:30 +0800

    first commit

---
+++ ReactMVC01/README.md
@@ -0,0 +1,3 @@
+﻿checkout this great link at
+https://dev.to/dance2die/setting-up-a-react-environment-for-aspnet-mvc-44la
+
---
+++ ReactMVC01/Scripts/Home/react/index.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+	<>
+		<h1>React in ASP.NET MVCwhat the fick!</h1>
+		<div>Hello React World</div>
+	</>
+);
+
+render(<App />, document.getElementById('app'));
